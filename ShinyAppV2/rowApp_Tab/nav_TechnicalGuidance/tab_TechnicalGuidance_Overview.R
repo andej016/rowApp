f5dc5ea60c7c03b43da79e882b97207fe0cea793
joe.anderson <joe.anderson@mismarine.com>
2018-05-10 15:45:02 +0100

    Technical Summary disclaimer

--- ShinyAppV2/rowApp_Tab/nav_TechnicalGuidance/tab_TechnicalGuidance_Overview.R
+++ ShinyAppV2/rowApp_Tab/nav_TechnicalGuidance/tab_TechnicalGuidance_Overview.R
@@ -18,5 +18,19 @@ rowApp_Tab_TechnicalGuidance_Overview <-
              )
            ),
            mainPanel(
+             HTML(
+             "Rowing Techinique takes many forms. In this app we have attempted to 
+             subset the rowing stroke to provide technical insight 
+             on each element of the stroke.<br>
+             There are many articles available in the menu expressing opinions about 
+             these different elements.<br>
+             It should be noted that every coach and athelete has a different opinion 
+             about what the correct or most effective technique is. This site is intended 
+             to aid, advise and potentionally confuse athletes and coaches. Please read over 
+             the material and utilise the information as you see fit.<br>
+             <br>
+             Finally, if you have anything to add or further technical guidance to give please 
+             email this to us."
+             )
            )
   )
